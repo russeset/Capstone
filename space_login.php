ea25998688fd8059d6d5f1a9f517484e0ec27c5f
robertAHC <48092903+robertAHC@users.noreply.github.com>
2020-04-08 15:51:47 -0400

    space_login.php V3

--- space_login.php
+++ space_login.php
@@ -17,13 +17,27 @@
 	body {
 		font-family: 'Barlow', sans-serif;
 		text-align: center;
+		opacity: 0; 
+		transition: opacity 3s;
+		text-transform: uppercase;
 		/* Background from https://uigradients.com/ */
 		background: -webkit-linear-gradient(to right, #FFFFFF, #6DD5FA, #2980B9);  /* Chrome 10-25, Safari 5.1-6 */
 		background: linear-gradient(to right, #FFFFFF, #6DD5FA, #2980B9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 	}
+
+	img {
+			width: 200px;
+			height: 155px;
+			opacity: 0.8;
+			padding-top: 35px;
+			float: center;	
+		}
 	</style>
 </head>
-<body>
+
+<body onload="document.body.style.opacity='1'">
+	<img src='spacevulture.png' >
+
 <?php
 ////////////////////////////// BEGANING OF THE PROJECT ////////////////////////////////////
     
@@ -46,17 +60,27 @@ $result = mysqli_query($connection, $query);
 $row = mysqli_fetch_array($result);
 $count_row=mysqli_num_rows($result); 
 
+// Checking for login and password to be correct
 if (($row['username']== $login) && ($row['password']== $password))
   {
 	session_start();
-	echo "<h1>Hello World</h1>";
 	$username = $row["first_name"];
-	echo "<h1>Welcome user $username </h1>";
+	echo "<h1>Welcome $username </h1>";
+
+	// Continue to work here...
   }
 
+  // if not matches occur, send message.
   else {
-	  echo "conection failed";
+	  echo "
+			 <div <body style='padding-top: 20px'>
+			 	<h4> Sorry username or password do not match to our records!</h4>
+				<h4> Usernane: $login ---- Password: $password</h4>
+				<h4> Click <a href='Capstone_Login.html'> Here<a> to login again.</h4>
+			 </div> 
+	  ";
   }
+
 ?>
 </body>
 </html>
